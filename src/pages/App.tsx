dd83d973f76f123c06bf7e554c558cd42fc2d2c8
SeaHuyty <seahuyty4444@gmail.com>
2026-04-02 14:49:22 +0700

    Prepare folder structure for development

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <p className="text-8xl font-bold text-blue-500">
+        Tailwind is working
+      </p>
+    </div>
+  )
+}
+
+export default App
